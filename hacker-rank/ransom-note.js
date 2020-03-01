a53ca9ba252b8920ca8ddaec478cc08572e79671
stefan hranek <hranekstefan@gmail.com>
2020-02-29 20:43:20 -0800

    HackerRank - ransom-note now passes all tests

--- hacker-rank/ransom-note.js
+++ hacker-rank/ransom-note.js
@@ -1,25 +1,15 @@
 function checkMagazine(magazine, note) {
-  // console.log(magazine, note)
   let answer;
-  let noteCopy = [];
-  let counter = 0;
   for (let i = 0; i < note.length; i++) {
-    for (let j = 0; j < magazine.length; j++) {
-      if (note[i] === magazine[j]) {
-        noteCopy.push(magazine.splice(j, 1));
-        j--;
-        // console.log(noteCopy)
-        // console.log(note)
-        // console.log(magazine)
-        counter ++;
-      }
+    if (magazine.includes(note[i])) {
+      let index = magazine.indexOf(note[i]);
+      magazine.splice(index, 1);
+      answer = true;
+    } else {
+      answer = false;
+      break;
     }
   }
-  noteCopy = [].concat.apply([], noteCopy);
-  // console.log(noteCopy, note)
-  counter === note.length ? (answer = "Yes") : (answer = "No");
+  answer ? (answer = "Yes") : (answer = "No");
   console.log(answer);
 }
-
-// As of now, 7/22 test cases failed
-//  there is an issue where checking two objects with the same values is returning false
