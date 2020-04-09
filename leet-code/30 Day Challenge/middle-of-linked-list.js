107dc7de116d39be56c3b8454bab576640ba58bb
stefan hranek <hranekstefan@gmail.com>
2020-04-08 21:33:53 -0700

    Solved day 8

---
+++ leet-code/30 Day Challenge/middle-of-linked-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
+
+// Used the slow / fast method for determining the middle of the list
+// Fast goes twice the rate as slow, so slow will return the middle once fast reaches the end of the list
